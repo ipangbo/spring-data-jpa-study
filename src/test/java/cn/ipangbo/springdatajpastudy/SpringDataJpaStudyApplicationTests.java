3ef08ce40dbdf0fb947c6a334a805d3f6676245d
庞礴 <ipangbo@Hackintosh.local>
2022-11-18 12:00:40 +0800

    Initial commit

---
+++ src/test/java/cn/ipangbo/springdatajpastudy/SpringDataJpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ipangbo.springdatajpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
